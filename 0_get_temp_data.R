21f8b524a7ccf83e697fc4f4cd7367fcddd5a13f
magali17 <magalipdx@gmail.com>
2024-03-07 11:50:43 -0800

    added code to download online temp files

---
+++ 0_get_temp_data.R
@@ -0,0 +1,95 @@
+
+# about terra library: https://cran.r-project.org/web/packages/terra/terra.pdf 
+
+########################################################################################################################
+# SETUP
+########################################################################################################################
+# Clear workspace of all objects and unload all extra (non-base) packages
+rm(list = ls(all = TRUE))
+if (!is.null(sessionInfo()$otherPkgs)) {
+  res <- suppressWarnings(
+    lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=""),
+           detach, character.only=TRUE, unload=TRUE, force=TRUE))
+}
+
+knitr::opts_chunk$set(echo = F, 
+                      cache=F, cache.comments = F, 
+                      message = F, warning = F, 
+                      tidy.opts=list(width.cutoff=60), tidy=TRUE,
+                      fig.height = 8, fig.width = 8)  
+
+pacman::p_load(#EpiNOAA, #"error epinoa not avaialble for this version of R"
+  terra, # for raster files. updated/maintained version of 'raster' library 
+  tidyverse)
+
+# where temp files are stored in this project
+dt_path <- file.path("data", "raw", "temp")
+if(!dir.exists(dt_path)) {dir.create(dt_path)}
+
+
+########################################################################################################################
+# FUNCTIONS
+########################################################################################################################
+
+ 
+
+########################################################################################################################
+# DOWNLOAD DATA
+########################################################################################################################
+# go here: https://www.ncei.noaa.gov/products/land-based-station/nclimgrid-daily 
+# select FTP grids/area averages
+# **year-specific files are here: https://www.ncei.noaa.gov/data/nclimgrid-daily/access/grids/ 
+# documentation 1: https://www.ncei.noaa.gov/pub/data/daily-grids/v1-0-0/nclimgrid-daily_v1-0-0_readme-ftp.txt # 
+# documentaiton 2: https://www.ncei.noaa.gov/pub/data/daily-grids/v1-0-0/nclimgrid-daily_v1-0-0_user-guide.pdf 
+
+yrs <- c(2013:2018)
+months <- c(1:12) %>% str_pad(2, pad="0")
+
+# one year at a time
+## yr= yrs[2]
+lapply(yrs, function(yr) {
+  # create year-month file names (how these are labeled online)
+  file_names <- paste0("ncdd-", yr, months, "-grd-scaled.nc")
+  
+  # download 1 file at a time, if it doesn't already exist
+  ## f=file_names[1]
+  lapply(file_names, function(f){
+    file_path <- file.path(dt_path, f)
+    
+    if(!file.exists(file_path)) {
+      root_dir <- "https://www.ncei.noaa.gov/pub/data/daily-grids/v1-0-0/grids"
+      this_file <- paste0(root_dir, "/", yr, "/", f)
+      download.file(this_file, file_path)
+      }
+    })
+  })
+
+# # works!
+# test0 <- rast(file_path)
+# plot(test0)
+
+########################################################################################################################
+# UPLOAD RASTER FILES
+########################################################################################################################
+
+# upload raster files to R
+temp1312 <- rast(file.path(dt_path, "ncdd-201312-grd-scaled.nc"))
+
+
+# # summarize the data
+# temp
+# plot(temp$tmax_1)
+
+
+
+
+########################################################################################################################
+# CALCULATE TEMPERATURE AT ??EXACT LOCATIONS
+########################################################################################################################
+# SEE 0.2_crop_proximity_variables.R for example spatial code
+
+
+
+
+
+
